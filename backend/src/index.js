649073a7d545aac6009ac7af9f41cd46a7dad8f8
radhikagupta25 <radhikaaaguptaaa25@gmail.com>
2026-07-06 18:56:25 +0530

    Connect DB

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+import connectDB from "./db/index.js";
+import app from "./app.js";
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is listening at port ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log(`Mongo Atlas connection failed! Error:${err}`);
+    })
